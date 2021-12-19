406ace5dd3d3b4e212028f61385263597c576ec5
Gmyrl <abramenko.r.l@gmail.com>
2021-12-19 19:15:00 +0700

    Added db and python scripts

---
+++ script.py
@@ -0,0 +1,14 @@
+import sqlite3 as sq
+
+conn = sq.connect('Sheetboard.sqlite')
+
+cursor = conn.cursor()
+cursor.execute("delete from user;")
+cursor.execute("delete from note;")
+cursor.execute("delete from tag;")
+cursor.execute("delete from note_tags;")
+cursor.execute("insert into user(email, password) values('abramenko.r.l@gmail.com', 'QwErTy!');")
+conn.commit()
+cursor.execute("SELECT id, email, password FROM user where id=1;")
+results = cursor.fetchall()
+print(results) 
